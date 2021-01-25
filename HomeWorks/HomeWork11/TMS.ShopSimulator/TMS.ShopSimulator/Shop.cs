989697e9ff961dbe9eaa8c58e0ffdefc42cc7e6a
silakovvv <mr.silakoff27@gmail.com>
2021-01-25 22:19:03 +0300

    Add all changes from Lesson 12

--- HomeWorks/HomeWork11/TMS.ShopSimulator/TMS.ShopSimulator/Shop.cs
+++ HomeWorks/HomeWork11/TMS.ShopSimulator/TMS.ShopSimulator/Shop.cs
@@ -2,65 +2,97 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace TMS.ShopSimulator
 {
     internal class Shop
     {
         private PeopleGenerator peopleGenerator;
-        private ConcurrentQueue<Person> processingQueue;
-        private List<Thread> processors;
+        private Queue<Cashier> cashierQueue;
+        private List<Cashier> allCashiers;
+        private List<Task> currentTasks;
+
         private bool isOpen;
 
         public Shop(PeopleGenerator peopleGenerator, int cashierNumber)
         {
             this.peopleGenerator = peopleGenerator;
-            this.processingQueue = new ConcurrentQueue<Person>();
-
-            var processors = new List<Thread>();
+            this.cashierQueue = new Queue<Cashier>();
+            this.allCashiers = new List<Cashier>();
+            this.currentTasks = new List<Task>();
             for (int i = 0; i < cashierNumber; i++)
             {
-                processors.Add(new Thread(ProcessPeople));
+                allCashiers.Add(new Cashier());
             }
-            this.processors = processors;
         }
 
         internal void Open()
         {
             Console.WriteLine("Shop is opening...");
             isOpen = true;
-            for (int i=1; i<=processors.Count; i++)
+            for (int i=0; i < allCashiers.Count; i++)
             {
-                processors[i-1].Start(i);
-                Console.WriteLine($"Cachier {i} is opened.");
+                EnqueueCashier(allCashiers[i]);
+                Console.WriteLine($"Cachier {allCashiers[i].Name} is opened.");
             }
         }
 
         internal void Close()
         {
             isOpen = false;
+            Task.WaitAll(currentTasks.ToArray());
         }
 
         internal void EnterShop()
         {
-            processingQueue.Enqueue(peopleGenerator.GetPerson());
+            if (isOpen)
+            {
+                // public delegate void WaitCallback(object state);
+                var task = Task.Run(() => ProcessPerson(peopleGenerator.GetPerson()));
+                currentTasks.Add(task);
+                //task.ContinueWith(t => currentTasks.Remove(task));
+            }
         }
 
-        private void ProcessPeople(object obj)
+        private void ProcessPerson(Person person)
         {
-            while (isOpen)
+            if (isOpen)
             {
-                while (!this.processingQueue.IsEmpty)
+                Cashier cashier;
+                while (!TryDequeueCashier(out cashier))
                 {
-                    if (processingQueue.TryDequeue(out var person))
-                    {
-                        Console.WriteLine($"Cachier {obj} is processing {person.Name}...");
-                        Thread.Sleep(person.TimeToProcess);
-                        Console.WriteLine($"Cachier {obj} is processed {person.Name}.");
-                    }
+                    Thread.Sleep(100);
+                }
+
+                var timeToProcess = cashier.TimeToProcess + person.TimeToProcess;
+                Console.WriteLine($"Cashier {cashier.Name} is processing {person.Name} on thread {Thread.CurrentThread.ManagedThreadId}...");
+                Thread.Sleep(timeToProcess);
+                EnqueueCashier(cashier);
+            }
+            Console.WriteLine($"Cachier {Thread.CurrentThread.ManagedThreadId} is exitting.");
+        }
+
+        private void EnqueueCashier(Cashier cashier)
+        {
+            lock(cashierQueue)
+            {
+                cashierQueue.Enqueue(cashier);
+            }
+        }
+
+        private bool TryDequeueCashier(out Cashier cashier)
+        {
+            cashier = null;
+            lock (this.cashierQueue)
+            {
+                if (cashierQueue.Count > 0)
+                {
+                    cashier = this.cashierQueue.Dequeue();
+                    return true;
                 }
             }
-            Console.WriteLine($"Cachier {obj} is closed.");
+            return false;
         }
     }
 }
