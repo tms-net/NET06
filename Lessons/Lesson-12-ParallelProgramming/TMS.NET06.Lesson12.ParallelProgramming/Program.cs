276ceec990a31f5e8d14949adbfc16e3ca3baff5
Dzmitry.Andryienka <Dzmitry.Andryienka@askapplications.com>
2021-01-25 18:36:55 +0300

    Add Lesson 12 ParallelProgramming

---
+++ Lessons/Lesson-12-ParallelProgramming/TMS.NET06.Lesson12.ParallelProgramming/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace TMS.NET06.Lesson12.ParallelProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!!!");
+        }
+    }
+}
