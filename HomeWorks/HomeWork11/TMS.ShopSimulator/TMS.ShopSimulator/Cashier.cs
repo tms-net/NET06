df9a6604c69139d356b465377a89b02377550a72
silakovvv <mr.silakoff27@gmail.com>
2021-01-25 22:19:03 +0300

    Add all changes from Lesson 12

---
+++ HomeWorks/HomeWork11/TMS.ShopSimulator/TMS.ShopSimulator/Cashier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TMS.ShopSimulator
+{
+    internal class Cashier
+    {
+        private static Random rnd = new Random();
+
+        public int TimeToProcess { get; }
+        public string Name { get; }
+
+        public Cashier()
+        {
+            this.TimeToProcess = rnd.Next(3000);
+            this.Name = Guid.NewGuid().ToString("N");
+        }
+
+    }
+}
