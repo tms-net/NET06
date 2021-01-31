233f729ac6f8a82be5d26bee24f46c01117d37e2
Tatsiana Malinina <63168842+molliny42@users.noreply.github.com>
2021-01-31 18:24:37 +0300

    FileService: 1. Add abstract class FileService. 2.Implemented LocalFileService.

--- HomeWorks/Homework12Nbrb/TMS.Homework.Nbrb/FileLibrary/FileService.cs
+++ HomeWorks/Homework12Nbrb/TMS.Homework.Nbrb/FileLibrary/FileService.cs
@@ -2,7 +2,16 @@
 
 namespace FileLibrary
 {
-    public class FileService
+    public abstract class FileService<T>
     {
+        protected string _path;
+
+        public FileService(string path)
+        {
+            _path = path;
+        }
+
+        public abstract void Save(T data);
+        public abstract T Load();
     }
 }
---
+++ HomeWorks/Homework12Nbrb/TMS.Homework.Nbrb/FileLibrary/LocalFileService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace FileLibrary
+{
+    public class LocalFileService<T> : FileService<T>
+    {
+        public LocalFileService(string path) : base(path)
+        {
+        }
+
+        public override async void Save(T data)
+        {
+            using (FileStream fs = new FileStream(_path, FileMode.OpenOrCreate))
+            {
+                await JsonSerializer.SerializeAsync(fs, data);
+                Console.WriteLine("Data has been saved to file");
+            }
+        }
+
+        public override T Load()
+        {
+            T loadedData;
+            using (FileStream fs = new FileStream(_path, FileMode.OpenOrCreate))
+            {
+                byte[] array = new byte[fs.Length];
+                fs.Read(array, 0, array.Length);
+                loadedData = JsonSerializer.Deserialize<T>(array);
+            }
+
+            return loadedData;
+        }
+    }
+}
