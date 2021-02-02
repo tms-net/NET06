db3de4fadfbf1bfbae0ef586844936c06ab10f28
School <School@HOME->
2021-02-02 23:17:12 +0300

    APIClient: add documentation into Currency

--- HomeWorks/Homework12Nbrb/TMS.Homework.Nbrb/APILibrary/Models/Currency.cs
+++ HomeWorks/Homework12Nbrb/TMS.Homework.Nbrb/APILibrary/Models/Currency.cs
@@ -4,22 +4,89 @@ namespace APILibrary.Models
 {
     public class Currency
     {
-        public int Cur_ID { get; set; }// внутренний код
-        public int Cur_ParentID { get; set; }//– этот код используется для связи, при изменениях наименования, количества единиц к которому устанавливается курс белорусского рубля, буквенного, цифрового кодов и т.д.фактически одной и той же валюты*.
-        public int Cur_Code { get; set; }//– цифровой код
-        public string Cur_Abbreviation { get; set; } //– буквенный код
-        public string Cur_Name { get; set; } //– наименование валюты на русском языке
-        public string Cur_Name_Bel { get; set; } //– наименование на белорусском языке
-        public string Cur_Name_Eng { get; set; } //– наименование на английском языке
-        public string Cur_QuotName { get; set; }//наименование валюты на русском языке, содержащее количество единиц
-        public string Cur_QuotName_Bel { get; set; } //– наименование на белорусском языке, содержащее количество единиц
-        public string Cur_QuotName_Eng { get; set; }// – наименование на английском языке, содержащее количество единиц
-        public string Cur_NameMulti { get; set; } //– наименование валюты на русском языке во множественном числе
-        public string Cur_Name_BelMulti { get; set; } //– наименование валюты на белорусском языке во множественном числе*
-        public string Cur_Name_EngMulti { get; set; } //– наименование на английском языке во множественном числе*
-        public decimal Cur_Scale { get; set; } //– количество единиц иностранной валюты
-        public int Cur_Periodicity { get; set; } //– периодичность установления курса(0 – ежедневно, 1 – ежемесячно)
-        public DateTime? Cur_DateStart { get; set; } // – дата включения валюты в перечень валют, к которым устанавливается официальный курс бел.рубля
-        public DateTime? Cur_DateEnd { get; set; } //– дата исключения валюты из перечня валют, к которым устанавливается официальный курс бел.рубля
+        /// <summary>
+        /// Internal code (внутренний код)
+        /// </summary>
+        public int Cur_ID { get; set; }
+
+        /// <summary>
+        /// Parent ID code currency (этот код используется для связи, при изменениях наименования)
+        /// </summary>
+        public int Cur_ParentID { get; set; }
+
+        /// <summary>
+        /// Digital code (цифровой код)
+        /// </summary>
+        public int Cur_Code { get; set; }
+
+        /// <summary>
+        /// Letter code (буквенный код)
+        /// </summary>
+        public string Cur_Abbreviation { get; set; }
+
+        /// <summary>
+        /// Name of currency in Russian (наименование валюты на русском языке)
+        /// </summary>
+        public string Cur_Name { get; set; }
+
+        /// <summary>
+        /// Name of currency in Belorussian (наименование валюты на белорусском языке)
+        /// </summary>
+        public string Cur_Name_Bel { get; set; }
+
+        /// <summary>
+        /// Name of currency in English (наименование валюты на английском языке)
+        /// </summary>
+        public string Cur_Name_Eng { get; set; }
+
+        /// <summary>
+        /// The name of the currency in Russian, containing the number of units (наименование валюты на русском языке, содержащее количество единиц)
+        /// </summary>
+        public string Cur_QuotName { get; set; }
+
+        /// <summary>
+        /// The name of the currency in Belorussian, containing the number of units (наименование валюты на белорусском языке, содержащее количество единиц)
+        /// </summary>
+        public string Cur_QuotName_Bel { get; set; }
+
+        /// <summary>
+        /// The name of the currency in English, containing the number of units (наименование валюты на английском языке, содержащее количество единиц)
+        /// </summary>
+        public string Cur_QuotName_Eng { get; set; }
+
+        /// <summary>
+        /// The name of the currency in Russian in the plural (наименование валюты на русском языке во множественном числе)
+        /// </summary>
+        public string Cur_NameMulti { get; set; }
+
+        /// <summary>
+        /// The name of the currency in Belorussian in the plural (наименование валюты на белорусском языке во множественном числе)
+        /// </summary>
+        public string Cur_Name_BelMulti { get; set; }
+
+        /// <summary>
+        /// The name of the currency in English in the plural (наименование валюты на английском языке во множественном числе)
+        /// </summary>
+        public string Cur_Name_EngMulti { get; set; }
+
+        /// <summary>
+        /// Number of units of foreign currency (количество единиц иностранной валюты)
+        /// </summary>
+        public decimal Cur_Scale { get; set; }
+
+        /// <summary>
+        /// The frequency of setting currency (периодичность установления курса)
+        /// </summary>
+        public int Cur_Periodicity { get; set; }
+
+        /// <summary>
+        /// Date of inclusion of a currency in the list of currencies (дата включения валюты в перечень валют)
+        /// </summary>
+        public DateTime? Cur_DateStart { get; set; }
+
+        /// <summary>
+        /// Date of exclusion of a currency from the list of currencies (дата исключения валюты из перечня валют)
+        /// </summary>
+        public DateTime? Cur_DateEnd { get; set; }
     }
 }
