fa8ecc3d0cb359fa75043eae5c8728519d28e602
Dmitry Andrienko <mr.silakoff27@gmail.com>
2021-02-04 22:07:27 +0300

    Lesson-15-Reflection: Create boilerplate for MVConsole

---
+++ HomeWorks/Homework13/TMS.HomeWork.MVConsole/TMS.Homework.MVConsole.Service/CsvService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TMS.Homework.MVConsole.Service
+{
+    public class CsvService
+    {
+        private readonly string _rootPath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                "MVConsole");
+
+        void Persist<T>(IEnumerable<T> models)
+        {
+            var path = Path.Combine(_rootPath, DateTime.Now.ToString());
+            using (var fs = new FileStream(path, FileMode.Create))
+            {
+                foreach(var model in models)
+                {
+                    fs.Write(Encoding.UTF8.GetBytes(model.ToString()));
+                }
+            }
+        }
+    }
+}
