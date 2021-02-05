e3f9203b3885533a19bb485d75549673641d89e9
Kirill Banakh <66416263+InsanireDevices@users.noreply.github.com>
2021-02-05 12:00:23 +0300

    feat: Add CSVHelper package. Add method to search solution root folder.

--- HomeWorks/Homework13/TMS.HomeWork.MVConsole/TMS.HomeWork.MVConsole.App/Program.cs
+++ HomeWorks/Homework13/TMS.HomeWork.MVConsole/TMS.HomeWork.MVConsole.App/Program.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.IO;
+using TMS.Homework.MVConsole.Service;
 
 Console.WriteLine("Hello MVC!!!");
+var csvService = new CsvService();
+//csvService.Persist<int>(null);
+Console.WriteLine(csvService.GetSolutionRootFolder().FullName);
+//Console.WriteLine(csvService.SaveToCSV());
+Console.ReadLine();
 
--- HomeWorks/Homework13/TMS.HomeWork.MVConsole/TMS.Homework.MVConsole.Service/CsvService.cs
+++ HomeWorks/Homework13/TMS.HomeWork.MVConsole/TMS.Homework.MVConsole.Service/CsvService.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace TMS.Homework.MVConsole.Service
@@ -11,16 +14,40 @@ namespace TMS.Homework.MVConsole.Service
                 Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
                 "MVConsole");
 
-        void Persist<T>(IEnumerable<T> models)
+        public void Persist<T>(IEnumerable<T> models)
         {
             var path = Path.Combine(_rootPath, DateTime.Now.ToString());
             using (var fs = new FileStream(path, FileMode.Create))
             {
-                foreach(var model in models)
+                foreach (var model in models)
                 {
                     fs.Write(Encoding.UTF8.GetBytes(model.ToString()));
                 }
             }
+        }
+
+        /// <summary>
+        /// Method that get`s solution root folder
+        /// </summary>
+        /// <param name="path">Let it be null if you are looking for your current solustion path</param>
+        /// <returns>Solution path</returns>
+        public DirectoryInfo GetSolutionRootFolder(string path = null)
+        {
+            var solutionPath = new DirectoryInfo(path ?? Directory.GetCurrentDirectory());
+            while (solutionPath != null && !solutionPath.GetFiles("*.sln").Any())
+            {
+                solutionPath = solutionPath.Parent;
+            }
+            return solutionPath;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="models"></param>
+        public void SaveToCSV<T> (IEnumerable<T> models, string path = null)
+        {
+
         }
     }
 }
