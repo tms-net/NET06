d0126783a0a6381ca1c152fe16b994c095c8fae0
volha-77 <volha-77@mail.ru>
2021-02-07 17:38:58 +0300

    test

--- HomeWorks/Homework13/TMS.HomeWork.MVConsole/TMS.HomeWork.MVConsole.App/Program.cs
+++ HomeWorks/Homework13/TMS.HomeWork.MVConsole/TMS.HomeWork.MVConsole.App/Program.cs
@@ -1,4 +1,18 @@
 ﻿using System;
+using TMS.Homework.MVConsole.UI;
+using TMS.HomeWork.MVConsole.App;
 
 Console.WriteLine("Hello MVC!!!");
+
+UI ui = new UI();
+MyClass myClass = new MyClass();
+//ui.View(myClass);
+
+//ui.Edit(myClass);
+var myClass1 = ui.Edit<MyClass>();
+Console.WriteLine("Вернулись==========");
+Console.WriteLine($"myClass.MyProperty1  {myClass1.MyProperty1}");
+Console.WriteLine($"myClass.MyProperty2 {myClass1.MyProperty2}");
+
+
 
--- HomeWorks/Homework13/TMS.HomeWork.MVConsole/TMS.Homework.MVConsole.UI/UI.cs
+++ HomeWorks/Homework13/TMS.HomeWork.MVConsole/TMS.Homework.MVConsole.UI/UI.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 namespace TMS.Homework.MVConsole.UI
 {
@@ -6,13 +7,122 @@ namespace TMS.Homework.MVConsole.UI
     {
         public void View<T>(T model)
         {
-            Console.WriteLine(model?.ToString());
+            // // Загружаем библиотеку классов
+            //Assembly asm = Assembly.LoadFrom("c:\\Users\\Admin\\source\repos\\TMS.Homework.Nbrb\\Nbrb\\bin\\Debug\\net5.0\\Nbrb.exe");
+            //// Находим типы содержащиеся в fontinfo.dll
+            //Type[] alltypes = asm.GetTypes();
+            //Console.WriteLine("*** Найденые типы ***\n");
+            //foreach (Type t1 in alltypes)
+            //    Console.WriteLine("-> " + t1.Name);
+            //Console.WriteLine();
+
+            //// Выбираем последний класс
+            //Type temp = alltypes[alltypes.Length - 1];
+            //Console.WriteLine("Используем класс " + temp.Name + "\n");
+
+            //// Отображаем сведения о конструкторах
+            //Console.WriteLine("*** Конструкторы ***\n");
+            //ConstructorInfo[] ci = temp.GetConstructors();
+            //foreach (ConstructorInfo c in ci)
+            //{
+            //    Console.Write("-> " + c.Name + "(");
+            //    // Выводим параметры
+            //    ParameterInfo[] p = c.GetParameters();
+            //    for (int i = 0; i < p.Length; i++)
+            //    {
+            //        Console.Write(p[i].ParameterType.Name + " " + p[i].Name);
+            //        if (i + 1 < p.Length) Console.Write(", ");
+            //    }
+            //    Console.Write(")\n\n");
+            //}
+            //Console.WriteLine("===============================other");
+
+            Type t = typeof(T);
+            Console.WriteLine("*** Конструкторы ***\n");
+            ConstructorInfo[] ci = t.GetConstructors();
+            foreach (ConstructorInfo c in ci)
+            {
+                Console.Write("-> " + c.Name + "(");
+                // Выводим параметры
+                ParameterInfo[] p = c.GetParameters();
+                for (int i = 0; i < p.Length; i++)
+                {
+                    Console.Write(p[i].ParameterType.Name + " " + p[i].Name);
+                    if (i + 1 < p.Length) Console.Write(", ");
+                }
+                Console.Write(")\n\n");
+            }
+            // Получаем коллекцию методов
+            MethodInfo[] MArr = t.GetMethods(BindingFlags.DeclaredOnly |
+                                                              BindingFlags.Instance | BindingFlags.Public);
+            Console.WriteLine("*** Список методов класса {0} ***\n", model.ToString());
+
+            // Вывести методы
+            foreach (MethodInfo m in MArr)
+            {
+                Console.Write(" --> " + m.ReturnType.Name + " \t" + m.Name + "(");
+                // Вывести параметры методов
+                ParameterInfo[] p = m.GetParameters();
+                for (int i = 0; i < p.Length; i++)
+                {
+                    Console.Write(p[i].ParameterType.Name + " " + p[i].Name);
+                    if (i + 1 < p.Length) Console.Write(", ");
+                }
+                Console.Write(")\n");
+                if (m.Name == "MyMethod")
+                {
+                    Console.Write("\n вызов метода\n");
+                    object[] args = new object[1];
+                    args[0] = 9;
+                    m.Invoke(model, args);
+                }
+            }
+
+            Console.WriteLine("\n*** Реализуемые интерфейсы ***\n");
+            var im = t.GetInterfaces();
+            foreach (Type tp in im)
+                Console.WriteLine("--> " + tp.Name);
+            Console.WriteLine("\n*** Поля и свойства ***\n");
+            FieldInfo[] fieldNames = t.GetFields();
+            foreach (FieldInfo fil in fieldNames)
+                Console.Write("--> " + fil.ReflectedType.Name + " " + fil.Name + "\n");
+
+           
         }
 
         // TODO: learn generic type constraints
-        public T Edit<T>() where T: class, new()
+       // public T Edit<T>(T class1) where T: class, new()
+        public T Edit<T>() where T : class, new()
         {
-            return new T();
+            //return new T();
+            T instance = null;
+
+            Type type = typeof(T);
+
+            try
+            {
+                // Используем позднее связывание
+                instance = (T) Activator.CreateInstance(type);
+                Console.WriteLine("Объект создан!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            for (int i = 0; i < properties.Length; i++)
+            {
+                // properties[i].SetValue(i, " ");
+                Type propertyType = properties[i].PropertyType;
+                Console.WriteLine(properties[i].PropertyType.Name);
+                if (propertyType.Name == "Int32") { properties[i].SetValue(instance, 32); }
+                if (propertyType.Name == "String") { properties[i].SetValue(instance, "someString"); }
+                Console.WriteLine(properties[i].GetValue(instance));
+            }
+
+            return instance;
         }
     }
 }
