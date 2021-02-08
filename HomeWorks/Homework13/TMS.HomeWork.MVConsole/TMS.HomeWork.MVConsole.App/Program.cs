f549bdf5e971327dd4391f5edee0e56b30c4b501
Kirill Banakh <66416263+InsanireDevices@users.noreply.github.com>
2021-02-08 14:39:42 +0300

    feat: Add method SaveToCsv with 3 overloads.

--- HomeWorks/Homework13/TMS.HomeWork.MVConsole/TMS.HomeWork.MVConsole.App/Program.cs
+++ HomeWorks/Homework13/TMS.HomeWork.MVConsole/TMS.HomeWork.MVConsole.App/Program.cs
@@ -1,11 +1,28 @@
 ﻿using System;
-using System.IO;
+using System.Collections.Generic;
+using System.Threading;
 using TMS.Homework.MVConsole.Service;
 
+var record = new List<Foo>
+{
+    new Foo {ID = 1, Name = "One", Sex = "Male"},
+    new Foo {ID = 2, Name = "Two", Sex = "Female"}
+};
 Console.WriteLine("Hello MVC!!!");
 var csvService = new CsvService();
-//csvService.Persist<int>(null);
-Console.WriteLine(csvService.GetSolutionRootFolder().FullName);
-//Console.WriteLine(csvService.SaveToCSV());
+
+csvService.SaveToCSV(record);
+csvService.SaveToCSV(record, csvService.GetSolutionRootFolder().FullName);
+csvService.SaveToCSV(record, csvService.GetSolutionRootFolder().FullName, "MyFileName");
+try
+{
+    csvService.SaveToCSV(record, csvService.GetSolutionRootFolder().FullName, "MyFileName");
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+
+
 Console.ReadLine();
 
--- HomeWorks/Homework13/TMS.HomeWork.MVConsole/TMS.Homework.MVConsole.Service/CsvService.cs
+++ HomeWorks/Homework13/TMS.HomeWork.MVConsole/TMS.Homework.MVConsole.Service/CsvService.cs
@@ -1,10 +1,9 @@
-﻿using Microsoft.VisualBasic.CompilerServices;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace TMS.Homework.MVConsole.Service
 {
@@ -40,14 +39,123 @@ namespace TMS.Homework.MVConsole.Service
             }
             return solutionPath;
         }
+
         /// <summary>
         /// 
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="models"></param>
-        public void SaveToCSV<T> (IEnumerable<T> models, string path = null)
+        public void SaveToCSV<T>(IEnumerable<T> models)
         {
+            var path = GetSolutionRootFolder() + $"\\{DateTime.Now.ToString("dd-MM-yyyy hh-mm-ss")}.csv";
+            if (File.Exists(path))
+                Thread.Sleep(1000);
+            path = GetSolutionRootFolder() + $"\\{DateTime.Now.ToString("dd-MM-yyyy hh-mm-ss")}.csv";
+            var properties = typeof(T).GetProperties();
+            var stringBuilder = new StringBuilder();
+            using (var fileStream = new FileStream(path, FileMode.Create))
+            {
+                for (int i = 0; i < properties.Length; i++)
+                {
+                    fileStream.Write(Encoding.UTF8.GetBytes($"{properties[i].Name}"));
+                    if (i != properties.Length - 1)
+                        fileStream.Write(Encoding.UTF8.GetBytes(","));
+                }
+                fileStream.Write(Encoding.UTF8.GetBytes("\r\n"));
 
+                foreach (var m in models)
+                {
+                    for (int i = 0; i < properties.Length; i++)
+                    {
+                        fileStream.Write(Encoding.UTF8.GetBytes($"{m.GetType().GetProperty(properties[i].Name).GetValue(m, null)}"));
+                        if (i != properties.Length - 1)
+                            fileStream.Write(Encoding.UTF8.GetBytes(","));
+                    }
+                    fileStream.Write(Encoding.UTF8.GetBytes("\r\n"));
+                }
+            }
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="models"></param>
+        /// <param name="path"></param>
+        public void SaveToCSV<T>(IEnumerable<T> models, string path)
+        {
+            var pathToSave = path + $"\\{DateTime.Now.ToString("dd-MM-yyyy hh-mm-ss")}.csv";
+            if (File.Exists(pathToSave))
+                Thread.Sleep(1000);
+            pathToSave = null;
+            pathToSave = path + $"\\{DateTime.Now.ToString("dd-MM-yyyy hh-mm-ss")}.csv";
+            var properties = typeof(T).GetProperties();
+            var stringBuilder = new StringBuilder();
+            using (var fileStream = new FileStream(pathToSave, FileMode.Create))
+            {
+                for (int i = 0; i < properties.Length; i++)
+                {
+                    fileStream.Write(Encoding.UTF8.GetBytes($"{properties[i].Name}"));
+                    if (i != properties.Length - 1)
+                        fileStream.Write(Encoding.UTF8.GetBytes(","));
+                }
+                fileStream.Write(Encoding.UTF8.GetBytes("\r\n"));
+
+                foreach (var m in models)
+                {
+                    for(int i = 0; i < properties.Length; i++)
+                    {
+                        fileStream.Write(Encoding.UTF8.GetBytes($"{m.GetType().GetProperty(properties[i].Name).GetValue(m, null)}"));
+                        if (i != properties.Length-1)
+                            fileStream.Write(Encoding.UTF8.GetBytes(","));
+                    }
+                    fileStream.Write(Encoding.UTF8.GetBytes("\r\n"));
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="models"></param>
+        /// <param name="path"></param>
+        public void SaveToCSV<T>(IEnumerable<T> models, string path, string fileName)
+        {
+            path = path + $"\\{fileName}.csv";
+            
+            if(File.Exists(path))
+                throw new Exception("Файл с таким именем уже существует!");
+            
+            var properties = typeof(T).GetProperties();
+            var stringBuilder = new StringBuilder();
+            using (var fileStream = new FileStream(path, FileMode.Create))
+            {
+                for (int i = 0; i < properties.Length; i++)
+                {
+                    fileStream.Write(Encoding.UTF8.GetBytes($"{properties[i].Name}"));
+                    if (i != properties.Length - 1)
+                        fileStream.Write(Encoding.UTF8.GetBytes(","));
+                }
+                fileStream.Write(Encoding.UTF8.GetBytes("\r\n"));
+
+                foreach (var m in models)
+                {
+                    for (int i = 0; i < properties.Length; i++)
+                    {
+                        fileStream.Write(Encoding.UTF8.GetBytes($"{m.GetType().GetProperty(properties[i].Name).GetValue(m, null)}"));
+                        if (i != properties.Length - 1)
+                            fileStream.Write(Encoding.UTF8.GetBytes(","));
+                    }
+                    fileStream.Write(Encoding.UTF8.GetBytes("\r\n"));
+                }
+            }
+        }
+
+    }
+    public class Foo
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Sex { get; set; }
     }
 }
