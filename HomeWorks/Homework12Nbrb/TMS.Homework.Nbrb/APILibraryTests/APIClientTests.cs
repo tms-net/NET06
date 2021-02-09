fb6de30c0facbd7928e5008c2d0f60c80e2c0a93
Dzmitry.Andryienka <Dzmitry.Andryienka@askapplications.com>
2021-02-09 09:28:46 +0300

    Lesson-16-UnitTesting: add tests for nbrb

---
+++ HomeWorks/Homework12Nbrb/TMS.Homework.Nbrb/APILibraryTests/APIClientTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace APILibraryTests
+{
+	public class ApiClientTests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void Test1()
+		{
+			Assert.Pass();
+		}
+	}
+}
---
+++ HomeWorks/Homework12Nbrb/TMS.Homework.Nbrb/UILibraryTests/UIApplicationTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace UILibraryTests
+{
+	public class UIApplicationTests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void Test1()
+		{
+			Assert.Pass();
+		}
+	}
+}
