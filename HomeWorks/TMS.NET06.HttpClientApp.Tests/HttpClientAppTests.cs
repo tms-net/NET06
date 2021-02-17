1d12fb5c210bdc5d7b483d10eb9fe5e711952891
Dzmitry.Andryienka <Dzmitry.Andryienka@askapplications.com>
2021-02-17 12:44:49 +0300

    HomeWork-17-Http: Add http application sample and test to check ETag headers handling

---
+++ HomeWorks/TMS.NET06.HttpClientApp.Tests/HttpClientAppTests.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using Moq.Protected;
+using NUnit.Framework;
+
+namespace TMS.NET06.HttpClientApp.Tests
+{
+	public class HttpClientAppTests
+	{
+		[SetUp]
+		public void Setup()
+		{
+			//https://en.wikipedia.org/wiki/HTTP_ETag
+		}
+
+		[Test]
+		public async Task GetContentShouldCorrectlyHandleETagHeaders()
+		{
+			// arrange
+			var etagUri = new Uri("http://localhost/etag");
+			var etag = "\"686897696a7c876b7e\"";
+			var etagHeader = EntityTagHeaderValue.Parse(etag);
+			var handlerMock = new Mock<HttpMessageHandler>();
+			handlerMock.Protected()
+				.Setup<Task<HttpResponseMessage>>(
+					"SendAsync",
+					ItExpr.Is<HttpRequestMessage>(req =>
+						req.RequestUri == etagUri &&
+						req.Headers.IfNoneMatch.Contains(etagHeader)),
+					ItExpr.IsAny<CancellationToken>())
+				.ReturnsAsync(() => 
+					new HttpResponseMessage(HttpStatusCode.NotModified));
+			handlerMock.Protected()
+				.Setup<Task<HttpResponseMessage>>(
+					"SendAsync",
+					ItExpr.Is<HttpRequestMessage>(req =>
+						req.RequestUri == etagUri && !req.Headers.IfNoneMatch.Contains(etagHeader)),
+					ItExpr.IsAny<CancellationToken>())
+				.ReturnsAsync(() => new HttpResponseMessage
+				{
+					StatusCode = HttpStatusCode.OK,
+					Content = new StringContent($"{etag}_content"),
+					Headers = { ETag = etagHeader }
+				});
+			handlerMock.Protected()
+				.Setup<Task<HttpResponseMessage>>(
+					"SendAsync",
+					ItExpr.Is<HttpRequestMessage>(req =>
+						req.RequestUri != etagUri),
+					ItExpr.IsAny<CancellationToken>())
+				.ReturnsAsync(() => new HttpResponseMessage
+				{
+					StatusCode = HttpStatusCode.OK,
+					Content = new StringContent(Guid.NewGuid().ToString("N"))
+				});
+			var httpClientApp = new HttpClientApp(handlerMock.Object);
+
+			// act
+			var actualResponses = new[]
+			{
+				await httpClientApp.GetContentAsync(etagUri),
+				await httpClientApp.GetContentAsync(etagUri)
+			};
+
+			// assert
+			handlerMock.Protected().Verify(
+				"SendAsync",
+				Times.Exactly(1),
+				ItExpr.Is<HttpRequestMessage>(req =>
+					req.Method == HttpMethod.Get && req.RequestUri == etagUri && !req.Headers.IfNoneMatch.Any()),
+				ItExpr.IsAny<CancellationToken>());
+			CollectionAssert.AreEquivalent(new []
+			{
+				$"{etag}_content",
+				$"{etag}_content"
+			}, actualResponses);
+		}
+	}
+}
---
+++ HomeWorks/TMS.NET06.HttpClientApp/HttpClientApp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+[assembly:InternalsVisibleTo("TMS.NET06.HttpClientApp.Tests")]
+namespace TMS.NET06.HttpClientApp
+{
+	public class HttpClientApp
+	{
+		private readonly HttpClient _httpClient;
+
+		internal HttpClientApp(HttpMessageHandler handler)
+		{
+			_httpClient = new HttpClient(handler);
+		}
+
+		public Task<string> GetContentAsync(Uri uri)
+		{
+			return _httpClient.GetStringAsync(uri);
+		}
+	}
+}
