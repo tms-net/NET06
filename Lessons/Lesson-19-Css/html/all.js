fa6e4edc95b2c4198bc17a4282467f70a20bd289
Dzmitry.Andryienka <Dzmitry.Andryienka@askapplications.com>
2021-03-11 19:03:52 +0300

    Lesson-21-JS: add sample for modules

--- Lessons/Lesson-19-Css/html/all.js
+++ Lessons/Lesson-19-Css/html/all.js
@@ -1,2 +1,79 @@
 (function() {
+
+    // window.FieldValidator = FieldValidator;
+
 })()
+
+function RequiredValidator(element) {
+    if (!element) {
+        return true;
+    }
+    
+    switch(element.type) {
+        case "checkbox": return element.checked;
+        default: return !!element.value;
+    };
+}
+
+RequiredValidator.errorText = "is required";
+
+function RegExpValidator(element, regExp) {
+    return !element || !element.value || regExp.match(element.value);
+}
+
+class FieldValidator {
+    static Required = RequiredValidator;
+
+    static RegExp(regExp) {
+        var validator = (elem) => RegExpValidator(elem, regExp);
+        validator.errorText = "should be " + regExp;
+        return validator;
+    }
+
+    constructor(id, validators) {
+        this._id = id;
+        this._validators = validators;
+        this._isValid = true;
+    }
+
+    initialize() {
+        this._element = document.getElementById(this._id);
+        var wrapper = document.createElement("span");
+        this._element.replaceWith(wrapper);
+        wrapper.appendChild(this._element);
+    }
+
+    isValid() {
+        return this._isValid;
+    }
+
+    validate() {
+        this.clearErrors();
+        if (this._element && this._validators) {
+            this._isValid = true;
+            this._validators.forEach(validator => {
+                if (!validator(this._element)) {
+                    this.showError(this._element.name + " " + validator.errorText);
+                    this._isValid = false;
+                };
+            });
+        }
+    }
+
+    clearErrors() {
+        for (const elem of this._element.parentElement.children) {
+            if (elem.className == 'error') {
+                elem.remove();
+            }
+        }
+    }
+    
+    showError(error) {
+        var errorElem = document.createElement("span");
+        errorElem.className = "error";
+        errorElem.innerText = error;
+        this._element.parentElement.appendChild(errorElem);
+    }
+}
+
+//export {FieldValidator};
