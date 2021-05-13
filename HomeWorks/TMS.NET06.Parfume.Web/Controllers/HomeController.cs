be0cd8019fa638b5fee18150067e06333dd8400b
Egor <egor.xavr@gmail.com>
2021-05-13 22:03:38 +0300

    Lesson-38: Add MVC template for bootstrap theme

---
+++ HomeWorks/TMS.NET06.Parfume.Web/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TMS.NET06.Parfume.Web.Models;
+
+namespace TMS.NET06.Parfume.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var model = new HomePageViewModel
+            {
+                ClientImages = new[]
+                {
+                    "~/img/clients/client-1.png",
+                    "~/img/clients/client-2.png",
+                    "~/img/clients/client-3.png",
+                }
+            };
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HomeWorks/TMS.NET06.Parfume.Web/Models/HomePageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TMS.NET06.Parfume.Web.Models
+{
+    public class HomePageViewModel
+    {
+        public IEnumerable<string> ClientImages { get; set; }
+    }
+}
